d579c43b88cfdffef11c386540a4feda8f75bad2
gabriel.almeida <gabriel.almeida@lactec.com.br>
2025-02-24 17:23:21 -0300

    .

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 9090;
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Define o prefixo '/portfolio' para arquivos estáticos
+app.use('/portfolio', express.static(path.join(__dirname, 'public')));
 
+// Rota para a página inicial
 app.get('/portfolio', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Rota para lidar com 404 (não encontrado)
 app.use((req, res) => {
   res.status(404).send('<html><body><h1>404 Not Found</h1></body></html>');
 });
