fec4290e25553b62b2b74c204c7f4e83e446680b
gabriel.almeida <gabriel.almeida@lactec.com.br>
2025-02-24 13:09:26 -0300

    .

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 9090;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).send('<html><body><h1>404 Not Found</h1></body></html>');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
