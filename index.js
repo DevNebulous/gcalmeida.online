825f420cff1af546b340a7460731876167b4a088
Nbulos <gabrielcamilom15@gmail.com>
2025-09-15 15:45:29 -0300

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3002;
 
 // Define o prefixo '/portfolio' para arquivos estáticos
 app.use('/', express.static(path.join(__dirname, 'public')));
